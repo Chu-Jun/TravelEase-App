26d40acc0fd38b71612026fec30701e55ad070e4
Chu-Jun <ongchujun2002@gmail.com>
2025-03-27 11:09:03 +0800

    Show error if no routes available

--- components/TransportModeIndicator.tsx
+++ components/TransportModeIndicator.tsx
@@ -10,7 +10,8 @@ import {
   faTrain, 
   faBicycle,
   faSubway,
-  faArrowDown
+  faArrowDown,
+  faExclamationTriangle
 } from "@fortawesome/free-solid-svg-icons";
 
 const TransportModeIndicator = ({ transportMode }: any) => {
@@ -25,6 +26,7 @@ const TransportModeIndicator = ({ transportMode }: any) => {
     if (lowerMode.includes("train")) return faTrain;
     if (lowerMode.includes("subway") || lowerMode.includes("metro")) return faSubway;
     if (lowerMode.includes("bike") || lowerMode.includes("cycle")) return faBicycle;
+    if (lowerMode.includes("error")) return faExclamationTriangle;
     return faRoute; // Default icon
   };
 
@@ -36,6 +38,7 @@ const TransportModeIndicator = ({ transportMode }: any) => {
     if (lowerMode.includes("drive") || lowerMode.includes("car")) return {bg: "bg-red-600", text: "text-red-600"};
     if (lowerMode.includes("train") || lowerMode.includes("subway") || lowerMode.includes("metro")) return {bg: "bg-purple-600", text: "text-purple-600"};
     if (lowerMode.includes("bike") || lowerMode.includes("cycle")) return {bg: "bg-yellow-600", text: "text-yellow-600"};
+    if (lowerMode.includes("error")) return {bg: "bg-gray-600", text: "text-gray-600"};
     return {bg: "bg-gray-600", text: "text-gray-600"}; // Default color
   };
 
